b3952d3691f2b40f8940532f7b03ce400aae05fb
kevin_p <kprsd14@gmail.com>
2021-03-04 21:46:01 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('.form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email-address');
+const password = document.getElementById('password');
